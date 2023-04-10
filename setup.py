7819ddc66434bce53853dfd574c1be187e0efdcb
Burak Güngör <127878134+Burak-Gungor@users.noreply.github.com>
2023-04-10 08:08:50 +0300

    Create setup.py
    created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='TerminalColor',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/Burak-Gungor/TerminalColor/tree/main',
+    license='GNU GENERAL PUBLIC LICENSE',
+    author='burak gungor',
+    author_email='',
+    description='A simple no-overhead python color library'
+)
